e0a693ee7a024a6e9c16f62ce1117375e20bcb5a
childressdev <jcampbell@childressagency.com>
2019-01-16 16:47:51 -0500

    added normalize carousel heights

--- js/fedcon-scripts.js
+++ js/fedcon-scripts.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function($){
+  $('#testimonial-slider .carousel-inner .carousel-item').carouselHeights();
+
   $('#hero-carousel').on('slide.bs.carousel', function(slide){
     var oldSlideIndex = slide.from;
     var newSlideIndex = slide.to;
@@ -16,3 +18,32 @@ jQuery(document).ready(function($){
     });
   });
 });
+
+$.fn.carouselHeights = function () {
+  var items = $(this), //grab all slides
+    heights = [], //create empty array to store height values
+    tallest; //create variable to make note of the tallest slide
+
+  var normalizeHeights = function () {
+    items.each(function () { //add heights to array
+      heights.push($(this).height());
+    });
+    tallest = Math.max.apply(null, heights); //cache largest value
+    items.each(function () {
+      $(this).css('min-height', tallest + 'px');
+    });
+  };
+
+  normalizeHeights();
+
+  $(window).on('resize orientationchange', function () {
+    //reset vars
+    tallest = 0;
+    heights.length = 0;
+
+    items.each(function () {
+      $(this).css('min-height', '0'); //reset min-height
+    });
+    normalizeHeights(); //run it again 
+  });
+};
