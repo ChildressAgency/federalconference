c0cb665cbf90b4fcf9c91879c8de77f7f3d8f068
childressdev <jcampbell@childressagency.com>
2019-01-21 16:28:13 -0500

    removed article class

--- includes/article-section.php
+++ includes/article-section.php
@@ -1,4 +1,4 @@
-<article class="hp-article">
+<article>
   <h2 class="article-header <?php echo get_field($section_name . '_title_border_color') . ' ' . get_field($section_name . '_title_color'); ?>"><?php echo esc_html(get_field($section_name . '_title')); ?><small class="<?php echo get_field($section_name . '_subtitle_color'); ?>"><?php echo esc_html(get_field($section_name . '_subtitle')); ?></small></h2>
   <div class="article-body">
     <?php echo wp_kses_post(get_field($section_name . '_article')); ?>
