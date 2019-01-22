01e647b8bfed23116936ff4292387825a8b48128
childressdev <jcampbell@childressagency.com>
2019-01-22 09:53:02 -0500

    added

--- index.php
+++ index.php
@@ -5,11 +5,23 @@
     <?php if(have_posts()): ?>
       <?php if($wp_query->post_count > 1): ?>
         <?php //archive loop ?>
+        <div class="article-excerpt">
+          <h2 class="article-header title-border-none fedcon-green"><?php the_title(); ?></h2>
+          <?php the_excerpt(); ?>
+          <a href="<?php the_permalink(); ?>" class="read-more">Read More...</a>
+        </div>
       <?php else: ?>
         <?php //single result ?>
+        <article class="blog-article">
+          <h2 class="article-header title-border-none fedcon-green"><?php the_title(); ?></h2>
+          <div class="article-body">
+            <?php the_content(); ?>
+          </div>
+        </article>
       <?php endif; ?>
     <?php else: ?>
       <?php //no posts found ?>
+      <p><?php esc_html_e('Sorry, nothing was found.', 'fedcon'); ?>
     <?php endif; ?>
   </div>
 </main>
--- search.php
+++ search.php
@@ -6,7 +6,9 @@
     
     <?php if(have_posts()): while(have_posts()): the_post(); ?>
       <div class="article_excerpt">
-
+        <h2 class="article-header title-border-none fedcon-green"><?php the_title(); ?></h2>
+        <?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>" class="read-more">Read More...</a> 
       </div>
     <?php endwhile; else: ?>
       <p><?php esc_html_e('Sorry, nothing was found.', 'fedcon'); ?></p>
