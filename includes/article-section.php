98e14b9912161a58b3d882af13c0b33a2f49a470
childressdev <jcampbell@childressagency.com>
2019-01-22 12:37:23 -0500

    added esc

--- includes/article-section.php
+++ includes/article-section.php
@@ -1,5 +1,5 @@
 <article>
-  <h2 class="article-header <?php echo get_field($section_name . '_title_border_color') . ' ' . get_field($section_name . '_title_color'); ?>"><?php echo esc_html(get_field($section_name . '_title')); ?><small class="<?php echo get_field($section_name . '_subtitle_color'); ?>"><?php echo esc_html(get_field($section_name . '_subtitle')); ?></small></h2>
+  <h2 class="article-header <?php echo esc_attr(get_field($section_name . '_title_border_color')) . ' ' . esc_attr(get_field($section_name . '_title_color')); ?>"><?php echo esc_html(get_field($section_name . '_title')); ?><small class="<?php echo esc_attr(get_field($section_name . '_subtitle_color')); ?>"><?php echo esc_html(get_field($section_name . '_subtitle')); ?></small></h2>
   <div class="article-body">
     <?php echo wp_kses_post(get_field($section_name . '_article')); ?>
     <?php
