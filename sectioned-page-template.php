ff854acae2d1bd4cae34ec30bc7486acf401ced0
childressdev <jcampbell@childressagency.com>
2019-01-23 17:34:03 -0500

    added conditional for intro section

--- sectioned-page-template.php
+++ sectioned-page-template.php
@@ -4,12 +4,15 @@
  * Description: A page template with intro section. Each additional section alternates the background color.
  */
 get_header(); ?>
+
+<?php if(get_field('page_intro_title') || get_field('page_intro_subtitle') || get_field('page_intro_article')): ?>
 <section id="page-intro">
   <div class="container">
     <?php fedcon_article_section('page_intro'); ?>
   </div>
   <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/stripes-angle-bottom-left.png" class="bottom-stripes" alt="" />
 </section>
+<?php endif; ?>
 <?php 
   $page_sections = get_field('page_sections');
   if($page_sections){
