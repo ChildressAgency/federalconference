c1d1211c3bf9d78ca68d6de7bad148e004a493a0
childressdev <jcampbell@childressagency.com>
2019-01-23 12:41:05 -0500

    added expertise settings

--- fedcon-core-functionality/fedcon-core-functionality.php
+++ fedcon-core-functionality/fedcon-core-functionality.php
@@ -73,4 +73,5 @@ function fedcon_acf_fields(){
   require_once FEDCON_PLUGIN_DIR . '/includes/custom-fields/fedcon-acf-about-page-settings.php';
   require_once FEDCON_PLUGIN_DIR . '/includes/custom-fields/fedcon-acf-careers-page-settings.php';
   require_once FEDCON_PLUGIN_DIR . '/includes/custom-fields/fedcon-acf-contact-page-settings.php';
+  require_once FEDCON_PLUGIN_DIR . '/includes/custom-fields/fedcon-acf-expertise-settings.php';
 }
