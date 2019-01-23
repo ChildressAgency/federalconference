59bc597ef68b9447f8848cde1a7824bb3a0eeeea
childressdev <jcampbell@childressagency.com>
2019-01-23 14:33:18 -0500

    changed to more generic layout

--- page.php
+++ page.php
@@ -1,18 +1,16 @@
 <?php get_header(); ?>
-<section id="page-intro">
-  <div class="container">
-    <?php fedcon_article_section('page_intro'); ?>
-  </div>
-  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/stripes-angle-bottom-left.png" class="bottom-stripes" alt="" />
-</section>
-<?php 
-  $page_sections = get_field('page_sections');
-  if($page_sections){
-    for($i = 0; $i < $page_sections; $i++){
-      echo '<section class="page-section">';
-        fedcon_article_section('page_section_' . $i);
-      echo '</section>';
-    }
-  }
-?>
+  <main id="main">
+    <div class="container">
+      <?php if(have_posts()): while(have_posts()): the_post(); ?>
+        <article class="blog-article">
+          <h2 class="article-header title-border-none fedcon-green"><?php the_title(); ?></h2>
+          <div class="article-body">
+            <?php the_content(); ?>
+          </div>
+        </article>
+      <?php endwhile; else: ?>
+        <p><?php esc_html_e('Sorry, nothing was found.', 'fedcon'); ?></p>
+      <?php endif; ?>
+    </div>
+  </main>
 <?php get_footer();
