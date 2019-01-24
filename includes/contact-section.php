ae528dd81999d89b92c11f8c04f0679b0ded6213
childressdev <jcampbell@childressagency.com>
2019-01-24 17:43:28 -0500

    added diff stripe img check

--- includes/contact-section.php
+++ includes/contact-section.php
@@ -5,13 +5,14 @@ if(get_field('use_settings_from_home_page')){
 else{
   $page_id = get_the_ID();
 }
+
   $contact_img_id = get_post_meta($page_id, 'contact_section_background_image', true);
   $contact_img = wp_get_attachment_image_src($contact_img_id, 'full');
   $contact_img_url = $contact_img[0];
   $contact_img_css = get_field('contact_section_background_image_css', $page_id);
 ?>
 <section id="start" style="background-image:url(<?php echo esc_url($contact_img_url); ?>); <?php echo $contact_img_css; ?>">
-  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/stripes-angle-top-left-white.png" class="img-fluid top-stripes" alt="" />
+  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/stripes-angle-top-left-<?php echo is_front_page() ? 'gray' : 'white'; ?>.png" class="img-fluid top-stripes" alt="" />
   <div class="content-center mx-auto text-center">
     <h2><?php echo esc_html(get_field('contact_section_title', $page_id)); ?></h2>
     <?php echo wp_kses_post(get_field('contact_section_content', $page_id)); ?>
